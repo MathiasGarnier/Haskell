06e5914b6ab558e2b860f0c6a9e3f9870afcf030
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-08-06 20:43:28 +0200

    Update multipleArrayElementAtIndex.hs

--- multipleArrayElementAtIndex.hs
+++ multipleArrayElementAtIndex.hs
@@ -1,6 +1,6 @@
 num = [[0, 1, 2, 3, 4, 5], [5, 4, 3, 2, 1, 0]]
 
-multipleArrayElementAtIndex arrayIndex index = num !! index !! index
+multipleArrayElementAtIndex arrayIndex index = num !! arrayIndex !! index
 
 multipleArrayElementAtIndex 1 1
 4
